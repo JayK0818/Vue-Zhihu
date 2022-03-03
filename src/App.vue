050180fc80abdcb63f31debbb66a0af9929f411a
JayK0720 <1129594783@qq.com>
2022-03-03 22:12:04 +0800

    navigation-bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
 <template>
-  <div id="nav">
-  </div>
   <router-view/>
 </template>
-
-<style lang='scss' scoped>
-body{
-  background:#f9f9f9;
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="home">
-    <div class="container">
-      <div class="col-md-8">hello world</div>
-      <div class="col-md-4">你好生活</div>
-    </div>
+    <navigation-bar :user='user'/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import NavigationBar, { UserProps } from '@/components/navigation-bar/index.vue'
+
+const user: UserProps = {
+  name: 'kyrie',
+  id: 1,
+  isLogin: false
+}
 
 export default defineComponent({
-  name: 'Home'
+  name: 'Home',
+  components: {
+    [NavigationBar.name]: NavigationBar
+  },
+  setup () {
+    return {
+      user
+    }
+  }
 })
 </script>
 
